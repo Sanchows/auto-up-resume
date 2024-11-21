d6ae1aa1a6f3a8537201a7402f9b2bd3f2e110f4
Alexander Sachenko <sanyasach@mail.ru>
2024-11-21 13:59:27 +0300

    async telegram send_message

--- telegram.py
+++ telegram.py
@@ -1,29 +1,32 @@
 import datetime
-import json
 import logging
 
-import requests
+import httpx
 
 import settings
 
 log = logging.getLogger(__name__)
 
 
-def send_message(chat_id: int, text: str):
+async def send_message(chat_id: int, text: str) -> list[httpx.Response]:
     log.info("Sending message to chat_id #%s", chat_id)
     text = (
         f"{(datetime.datetime.now(tz=datetime.UTC) + datetime.timedelta(hours=3)).strftime("%d.%m.%Y %H:%M:%S")}\n"
         f"{text}"
     )
     url = f"https://api.telegram.org/bot{settings.Envs.TELEGRAM_LOGGER_BOT_TOKEN}/sendMessage"
+    responses = []
     while text:
         message = text[:4090]
         text = text[4090:]
-        response = requests.post(
-            url=url,
-            data=json.dumps({"chat_id": chat_id, "text": message}),
-            headers={"Content-type": "application/json"},
-            timeout=10,
-        )
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                url=url,
+                json={"chat_id": chat_id, "text": message},
+                timeout=10,
+            )
         log.info("Sent message to chat #%s. Response: %r", chat_id, response)
         log.debug("Sent message to chat #%s. Response text: %r", chat_id, response.text)
+        responses.append(response)
+    log.info("Sent %s messages to chat_id: #%", len(responses), responses)
+    return responses
