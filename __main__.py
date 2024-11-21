05b4bf34f4e47dd3a5afb4b1569b51656b7ae4c6
Alexander Sachenko <sanyasach@mail.ru>
2024-11-21 13:42:24 +0300

    async requests (httpx)

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import traceback
 
@@ -8,15 +9,20 @@ from telegram import send_message
 
 log = logging.getLogger(__name__)
 
-configure_logging(level=settings.LOGGING_LOG_LEVEL)
 
-try:
-    log.info("Starting app")
-    run()
-except KeyboardInterrupt:
-    pass
-except Exception as e:
-    log.exception(e)
-    if settings.IS_ENABLED_TELEGRAM_LOGGER:
-        send_message(chat_id=int(settings.Envs.TELEGRAM_LOGGER_CHAT_ID), text=traceback.format_exc())
-log.info("Closing app")
+async def start():
+    configure_logging(level=settings.LOGGING_LOG_LEVEL)
+    try:
+        log.info("Starting app")
+        await run()
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        log.exception(e)
+        if settings.IS_ENABLED_TELEGRAM_LOGGER:
+            await send_message(chat_id=int(settings.Envs.TELEGRAM_LOGGER_CHAT_ID), text=traceback.format_exc())
+    log.info("Closing app")
+
+
+if __name__ == "__main__":
+    asyncio.run(start())
--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import logging
 
-import requests
+import httpx
 
 import settings
 
 log = logging.getLogger(__name__)
 
 
-def batch_update() -> requests.Response:
+async def batch_update() -> httpx.Response:
     log.info("Sending batch update request")
-    response = requests.post(
-        url=settings.BATCH_UPDATE_URL,
-        headers={
-            "User-Agent": settings.USER_AGENT,
-            "Accept": "application/json",
-            "X-Xsrftoken": settings.Envs.COOKIES_XSRF,
-            "Cookie": f"hhtoken={settings.Envs.COOKIES_HHTOKEN}; hhuid={settings.Envs.COOKIES_HHUID};",
-        },
-    )
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            url=settings.BATCH_UPDATE_URL,
+            headers={
+                "User-Agent": settings.USER_AGENT,
+                "Accept": "application/json",
+                "X-Xsrftoken": settings.Envs.COOKIES_XSRF,
+            },
+            cookies={
+                "hhtoken": settings.Envs.COOKIES_HHTOKEN,
+                "hhuid": settings.Envs.COOKIES_HHUID,
+            },
+            timeout=10,
+        )
     log.info(
         "Batch update response: %r. Status code: %s",
         response,
         response.status_code,
     )
     log.debug("Batch update response text: %s", response.text)
+    log.debug("Batch update response json: %s", response.json())
     return response
 
 
-def run():
+async def run():
     log.info("Running")
-    batch_update()
+    await batch_update()
