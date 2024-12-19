c2eb74e989c73e47e01133b31b080646e4c9e687
Alexander Sachenko <sanyasach@mail.ru>
2024-12-19 11:35:25 +0300

    AuthStrategy added @abstractmethod

--- auth/interfaces.py
+++ auth/interfaces.py
@@ -1,8 +1,9 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from hh_session import HHSession
 
 
 class AuthStrategy(ABC):
+    @abstractmethod
     async def get_hh_session(self) -> HHSession:
         pass
